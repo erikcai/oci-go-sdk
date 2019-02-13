49bf67dc1b5151f64a64ad80246bff63d65a22fb
Jason Yin <jason.yin@oracle.com>
2019-02-13 23:42:28 +0000

    revertdex300 and regen
    Squashed commit of the following:
    
    commit 5cf30132fbd8bc8576258748bc3bf6fb76cf3822
    Author: Jason Yin <jason.yin@oracle.com>
    Date:   Wed Feb 13 14:59:31 2019 -0800
    
        clean cache and regen
    
    commit 440f603a49de34608420923cf63f35c5542f730a
    Author: Jason Yin <jason.yin@oracle.com>
    Date:   Wed Feb 13 14:10:26 2019 -0800
    
        regen
    
    commit 55f007a90e820d76a3e0ff1e09ce6b9518e428dc
    Author: Jason Yin <jason.yin@oracle.com>
    Date:   Wed Feb 13 14:06:40 2019 -0800
    
        Revert "Merge pull request #349 in SDK/oci-go-sdk from dexreq-300 to preview"
    
        This reverts commit 3d5193eb2f762096c5e21e62b5773b4b30bc9bdb.

--- autotest/marketplace_applicationinstanceresource_client_auto_test.go
+++ autotest/marketplace_applicationinstanceresource_client_auto_test.go
@@ -1,101 +1,99 @@
 package autotest
 
 import (
-    "github.com/oracle/oci-go-sdk/marketplace"
-    "github.com/oracle/oci-go-sdk/common"
+	"github.com/oracle/oci-go-sdk/common"
+	"github.com/oracle/oci-go-sdk/marketplace"
 
-    "context"
-    "encoding/json"
-    "fmt"
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func createApplicationInstanceResourceClientWithProvider(p common.ConfigurationProvider, testConfig TestingConfig) (interface{}, error) {
-    
-    client, err := marketplace.NewApplicationInstanceResourceClientWithConfigurationProvider(p)
-    if testConfig.Endpoint != "" {
-        client.Host = testConfig.Endpoint
-    }else {
-        client.SetRegion(testConfig.Region)
-    }
-   return client, err
+
+	client, err := marketplace.NewApplicationInstanceResourceClientWithConfigurationProvider(p)
+	if testConfig.Endpoint != "" {
+		client.Host = testConfig.Endpoint
+	} else {
+		client.SetRegion(testConfig.Region)
+	}
+	return client, err
 }
-
-
 
 // IssueRoutingInfo tag="" email="" jiraProject="" opsJiraProject=""
 func TestApplicationInstanceResourceClientGetInstallConfiguration(t *testing.T) {
-    enabled, err := testClient.isApiEnabled("marketplace", "GetInstallConfiguration")
-    assert.NoError(t, err)
-    if !enabled {
-        t.Skip("GetInstallConfiguration is not enabled by the testing service")
-    }
+	enabled, err := testClient.isApiEnabled("marketplace", "GetInstallConfiguration")
+	assert.NoError(t, err)
+	if !enabled {
+		t.Skip("GetInstallConfiguration is not enabled by the testing service")
+	}
 
-    cc, err := testClient.createClientForOperation("marketplace", "ApplicationInstanceResource", "GetInstallConfiguration", createApplicationInstanceResourceClientWithProvider)
-    assert.NoError(t, err)
-    c := cc.(marketplace.ApplicationInstanceResourceClient)
+	cc, err := testClient.createClientForOperation("marketplace", "ApplicationInstanceResource", "GetInstallConfiguration", createApplicationInstanceResourceClientWithProvider)
+	assert.NoError(t, err)
+	c := cc.(marketplace.ApplicationInstanceResourceClient)
 
-    body, err := testClient.getRequests("marketplace", "GetInstallConfiguration")
-    assert.NoError(t, err)
+	body, err := testClient.getRequests("marketplace", "GetInstallConfiguration")
+	assert.NoError(t, err)
 
-    type GetInstallConfigurationRequestInfo struct {
-        ContainerId string
-        Request marketplace.GetInstallConfigurationRequest
-    }
+	type GetInstallConfigurationRequestInfo struct {
+		ContainerId string
+		Request     marketplace.GetInstallConfigurationRequest
+	}
 
-    var requests []GetInstallConfigurationRequestInfo
-    err = json.Unmarshal([]byte(body), &requests)
-    assert.NoError(t, err)
+	var requests []GetInstallConfigurationRequestInfo
+	err = json.Unmarshal([]byte(body), &requests)
+	assert.NoError(t, err)
 
-    var retryPolicy  *common.RetryPolicy
-    for i, req := range requests {
-        t.Run(fmt.Sprintf("request:%v", i), func(t *testing.T) {
-            retryPolicy = retryPolicyForTests()
-            req.Request.RequestMetadata.RetryPolicy =  retryPolicy
+	var retryPolicy *common.RetryPolicy
+	for i, req := range requests {
+		t.Run(fmt.Sprintf("request:%v", i), func(t *testing.T) {
+			retryPolicy = retryPolicyForTests()
+			req.Request.RequestMetadata.RetryPolicy = retryPolicy
 
-            response, err := c.GetInstallConfiguration(context.Background(), req.Request)
-            message, err := testClient.validateResult(req.ContainerId, req.Request, response, err)
-            assert.NoError(t, err)
-            assert.Empty(t, message, message)
-        })
-    }
+			response, err := c.GetInstallConfiguration(context.Background(), req.Request)
+			message, err := testClient.validateResult(req.ContainerId, req.Request, response, err)
+			assert.NoError(t, err)
+			assert.Empty(t, message, message)
+		})
+	}
 }
 
 // IssueRoutingInfo tag="" email="" jiraProject="" opsJiraProject=""
 func TestApplicationInstanceResourceClientGetTermsOfUses(t *testing.T) {
-    enabled, err := testClient.isApiEnabled("marketplace", "GetTermsOfUses")
-    assert.NoError(t, err)
-    if !enabled {
-        t.Skip("GetTermsOfUses is not enabled by the testing service")
-    }
+	enabled, err := testClient.isApiEnabled("marketplace", "GetTermsOfUses")
+	assert.NoError(t, err)
+	if !enabled {
+		t.Skip("GetTermsOfUses is not enabled by the testing service")
+	}
 
-    cc, err := testClient.createClientForOperation("marketplace", "ApplicationInstanceResource", "GetTermsOfUses", createApplicationInstanceResourceClientWithProvider)
-    assert.NoError(t, err)
-    c := cc.(marketplace.ApplicationInstanceResourceClient)
+	cc, err := testClient.createClientForOperation("marketplace", "ApplicationInstanceResource", "GetTermsOfUses", createApplicationInstanceResourceClientWithProvider)
+	assert.NoError(t, err)
+	c := cc.(marketplace.ApplicationInstanceResourceClient)
 
-    body, err := testClient.getRequests("marketplace", "GetTermsOfUses")
-    assert.NoError(t, err)
+	body, err := testClient.getRequests("marketplace", "GetTermsOfUses")
+	assert.NoError(t, err)
 
-    type GetTermsOfUsesRequestInfo struct {
-        ContainerId string
-        Request marketplace.GetTermsOfUsesRequest
-    }
+	type GetTermsOfUsesRequestInfo struct {
+		ContainerId string
+		Request     marketplace.GetTermsOfUsesRequest
+	}
 
-    var requests []GetTermsOfUsesRequestInfo
-    err = json.Unmarshal([]byte(body), &requests)
-    assert.NoError(t, err)
+	var requests []GetTermsOfUsesRequestInfo
+	err = json.Unmarshal([]byte(body), &requests)
+	assert.NoError(t, err)
 
-    var retryPolicy  *common.RetryPolicy
-    for i, req := range requests {
-        t.Run(fmt.Sprintf("request:%v", i), func(t *testing.T) {
-            retryPolicy = retryPolicyForTests()
-            req.Request.RequestMetadata.RetryPolicy =  retryPolicy
+	var retryPolicy *common.RetryPolicy
+	for i, req := range requests {
+		t.Run(fmt.Sprintf("request:%v", i), func(t *testing.T) {
+			retryPolicy = retryPolicyForTests()
+			req.Request.RequestMetadata.RetryPolicy = retryPolicy
 
-            response, err := c.GetTermsOfUses(context.Background(), req.Request)
-            message, err := testClient.validateResult(req.ContainerId, req.Request, response, err)
-            assert.NoError(t, err)
-            assert.Empty(t, message, message)
-        })
-    }
+			response, err := c.GetTermsOfUses(context.Background(), req.Request)
+			message, err := testClient.validateResult(req.ContainerId, req.Request, response, err)
+			assert.NoError(t, err)
+			assert.Empty(t, message, message)
+		})
+	}
 }
