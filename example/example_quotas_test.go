d5b8872e0fda891c9a70614160bd28ca5e7a5f56
Jason Yin <jason.yin@oracle.com>
2019-05-14 17:42:44 +0000

    DEX-6008 enable build lint for sample code, and fix sample errors
    Squashed commit of the following:
    
    commit e834a0bd17ef3d5872c203a6162515e940acf58d
    Author: Jason Yin <jason.yin@oracle.com>
    Date:   Tue May 14 10:32:01 2019 -0700
    
        enable build lint for sample code, and fix sample errors

--- example/example_quotas_test.go
+++ example/example_quotas_test.go
@@ -19,118 +19,118 @@ package example
 
 // Import necessary packages
 import (
-    "github.com/oracle/oci-go-sdk/common"   // For common OCI types
-    "github.com/oracle/oci-go-sdk/limits"   // For types and methods corresponding to Limits
-	"context"                               // To supply to the Quotas client while making requests
-	"fmt"                                   // To print to the console
+	"context"                             // To supply to the Quotas client while making requests
+	"fmt"                                 // To print to the console
+	"github.com/oracle/oci-go-sdk/common" // For common OCI types
+	"github.com/oracle/oci-go-sdk/limits" // For types and methods corresponding to Limits
 )
 
 // Creates a new Quota with the details given in createQuotaDetails
 func createQuota(client limits.QuotasClient, createQuotaDetails limits.CreateQuotaDetails) limits.CreateQuotaResponse {
 	var response limits.CreateQuotaResponse
-    response, err := client.CreateQuota(context.Background(), limits.CreateQuotaRequest{CreateQuotaDetails:createQuotaDetails})
-    if err != nil {
-        panic(err)
-    }
+	response, err := client.CreateQuota(context.Background(), limits.CreateQuotaRequest{CreateQuotaDetails: createQuotaDetails})
+	if err != nil {
+		panic(err)
+	}
 	return response
 }
 
 // Gets the Quota corresponding to given quotaId
 func getQuota(client limits.QuotasClient, quotaId string) limits.GetQuotaResponse {
 	var response limits.GetQuotaResponse
-    quotaIdStr := common.String(quotaId)
-    response, err := client.GetQuota(context.Background(), limits.GetQuotaRequest{QuotaId:quotaIdStr})
-    if err != nil {
-        panic(err)
-    }
+	quotaIdStr := common.String(quotaId)
+	response, err := client.GetQuota(context.Background(), limits.GetQuotaRequest{QuotaId: quotaIdStr})
+	if err != nil {
+		panic(err)
+	}
 	return response
 }
 
 // Lists Quotas under the Compartment corresponding to given compartmentId
 func listQuotas(client limits.QuotasClient, compartmentId string) limits.ListQuotasResponse {
 	var response limits.ListQuotasResponse
-    compartmentIdStr := common.String(compartmentId)
-    response, err := client.ListQuotas(context.Background(), limits.ListQuotasRequest{CompartmentId:compartmentIdStr})
-    if err != nil {
-        panic(err)
-    }
+	compartmentIdStr := common.String(compartmentId)
+	response, err := client.ListQuotas(context.Background(), limits.ListQuotasRequest{CompartmentId: compartmentIdStr})
+	if err != nil {
+		panic(err)
+	}
 	return response
 }
 
 // Updates the Quota corresponding to given quotaId with values given in updateQuotaDetails
 func updateQuota(client limits.QuotasClient, quotaId string, updateQuotaDetails limits.UpdateQuotaDetails) limits.UpdateQuotaResponse {
 	var response limits.UpdateQuotaResponse
-    quotaIdStr := common.String(quotaId)
-    response, err := client.UpdateQuota(context.Background(), limits.UpdateQuotaRequest{QuotaId:quotaIdStr, UpdateQuotaDetails:updateQuotaDetails})
-    if err != nil {
-        panic(err)
-    }
+	quotaIdStr := common.String(quotaId)
+	response, err := client.UpdateQuota(context.Background(), limits.UpdateQuotaRequest{QuotaId: quotaIdStr, UpdateQuotaDetails: updateQuotaDetails})
+	if err != nil {
+		panic(err)
+	}
 	return response
 }
 
 // Deletes the Quota corresponding to given quotaId
 func deleteQuota(client limits.QuotasClient, quotaId string) limits.DeleteQuotaResponse {
 	var response limits.DeleteQuotaResponse
-    quotaIdStr := common.String(quotaId)
-    response, err := client.DeleteQuota(context.Background(), limits.DeleteQuotaRequest{QuotaId:quotaIdStr})
-    if err != nil {
-        panic(err)
-    }
+	quotaIdStr := common.String(quotaId)
+	response, err := client.DeleteQuota(context.Background(), limits.DeleteQuotaRequest{QuotaId: quotaIdStr})
+	if err != nil {
+		panic(err)
+	}
 	return response
 }
 
 // ExampleQuotas runs an example demonstrating the use of OCI Golang SDK for managing Quotas
 func ExampleQuotas() {
 
-    // Initialize default config provider
-    configProvider := common.DefaultConfigProvider()
+	// Initialize default config provider
+	configProvider := common.DefaultConfigProvider()
 	if ok, err := common.IsConfigurationProviderValid(configProvider); !ok {
-	    panic(err)
+		panic(err)
 	}
 
-    // Initialize sample inputs
-    compartmentId, _ := configProvider.TenancyOCID()
-    name := "MyQuota"
-    description := "This is a sample Quota"
-    newDescription := "This is an updated Quota"
-    statements := []string{"Zero test-family quota 'test-quota-1' in tenancy"}
+	// Initialize sample inputs
+	compartmentId, _ := configProvider.TenancyOCID()
+	name := "MyQuota"
+	description := "This is a sample Quota"
+	newDescription := "This is an updated Quota"
+	statements := []string{"Zero test-family quota 'test-quota-1' in tenancy"}
 
-    // Initialize Quotas client
-    client, err := limits.NewQuotasClientWithConfigurationProvider(configProvider)
-    if err != nil {
-        panic(err)
-    }
+	// Initialize Quotas client
+	client, err := limits.NewQuotasClientWithConfigurationProvider(configProvider)
+	if err != nil {
+		panic(err)
+	}
 
-    // Create Quota
-    fmt.Println("Creating Quota")
-    createQuotaDetails := limits.CreateQuotaDetails{CompartmentId:&compartmentId, Name:&name, Description:&description, Statements:statements}
-    createResponse := createQuota(client, createQuotaDetails)
+	// Create Quota
+	fmt.Println("Creating Quota")
+	createQuotaDetails := limits.CreateQuotaDetails{CompartmentId: &compartmentId, Name: &name, Description: &description, Statements: statements}
+	createResponse := createQuota(client, createQuotaDetails)
 
-    // Get Quota
-    fmt.Println("Getting Quota")
-    getQuota(client, *createResponse.Quota.Id)
+	// Get Quota
+	fmt.Println("Getting Quota")
+	getQuota(client, *createResponse.Quota.Id)
 
-    // List Quotas
-    fmt.Println("Listing Quotas")
-    listResponse := listQuotas(client, compartmentId)
+	// List Quotas
+	fmt.Println("Listing Quotas")
+	listResponse := listQuotas(client, compartmentId)
 
-    // Update Quota
-    fmt.Println("Updating Quota")
-    quotaId := *listResponse.Items[0].Id
-    updateQuotaDetails := limits.UpdateQuotaDetails{Description:&newDescription}
-    updateQuota(client, quotaId, updateQuotaDetails)
+	// Update Quota
+	fmt.Println("Updating Quota")
+	quotaId := *listResponse.Items[0].Id
+	updateQuotaDetails := limits.UpdateQuotaDetails{Description: &newDescription}
+	updateQuota(client, quotaId, updateQuotaDetails)
 
-    // Delete Quota
-    fmt.Println("Deleting Quota")
-    deleteQuota(client, quotaId)
+	// Delete Quota
+	fmt.Println("Deleting Quota")
+	deleteQuota(client, quotaId)
 
-    fmt.Println("ExampleQuotas completed")
+	fmt.Println("ExampleQuotas completed")
 
-    // Output:
-    // Creating Quota
-    // Getting Quota
-    // Listing Quotas
-    // Updating Quota
-    // Deleting Quota
-    // ExampleQuotas completed
+	// Output:
+	// Creating Quota
+	// Getting Quota
+	// Listing Quotas
+	// Updating Quota
+	// Deleting Quota
+	// ExampleQuotas completed
 }
